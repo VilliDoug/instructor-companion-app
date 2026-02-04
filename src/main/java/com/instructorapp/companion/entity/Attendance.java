69ee015ae6df6391ff18b249cbb9408946cf6fd3
VilliDoug <villidoug@gmail.com>
2026-02-04 23:13:25 +0900

    「Attendance」エンティティ追加・参加回数情報。

---
+++ src/main/java/com/instructorapp/companion/entity/Attendance.java
@@ -0,0 +1,28 @@
+package com.instructorapp.companion.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendance")
+public class Attendance {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @Column(nullable = false)
+  private LocalDateTime attendanceDate;
+
+}
