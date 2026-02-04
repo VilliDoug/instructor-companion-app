4bc2e9bb896f12d1f9a41380fccdfd8f4ac8666e
VilliDoug <villidoug@gmail.com>
2026-02-04 23:55:44 +0900

    アプリを実行・JPA機能によりDBテーブル設定・JpaRepositoryを追加「実装未定」

--- src/main/java/com/instructorapp/companion/repository/MemberRepository.java
+++ src/main/java/com/instructorapp/companion/repository/MemberRepository.java
@@ -1,5 +1,10 @@
 package com.instructorapp.companion.repository;
 
-public class MemberRepository {
+import com.instructorapp.companion.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
