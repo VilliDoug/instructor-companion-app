0fa1882d95a99c2cb06d3b7a923dfb9730e34dc0
VilliDoug <villidoug@gmail.com>
2026-02-05 21:43:44 +0900

    「GET」・会員一覧検索(getAllMembers)、・IDで詳細取得　(getMemberDetails)

--- src/main/java/com/instructorapp/companion/controller/MemberController.java
+++ src/main/java/com/instructorapp/companion/controller/MemberController.java
@@ -1,5 +1,35 @@
 package com.instructorapp.companion.controller;
 
+import com.instructorapp.companion.dto.MemberDTO;
+import com.instructorapp.companion.dto.MemberDetailDTO;
+import com.instructorapp.companion.service.MemberService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+
+  private MemberService memberService;
+
+  public MemberController(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<MemberDTO>> getAllMembers() {
+    List<MemberDTO> members = memberService.getAllMembers();
+    return ResponseEntity.ok(members);
+  }
+
+  @GetMapping("/{id}/details")
+  public ResponseEntity<MemberDetailDTO> getMemberDetails(@PathVariable Long id) {
+    MemberDetailDTO details = memberService.getMemberDetails(id);
+    return ResponseEntity.ok(details);
+  }
 
 }
