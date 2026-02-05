896316e426c480ff3a5545ab9abc115e237c8d13
VilliDoug <villidoug@gmail.com>
2026-02-05 21:41:20 +0900

    findByMemberId・カスタムメソッド追加

--- src/main/java/com/instructorapp/companion/repository/AttendanceRepository.java
+++ src/main/java/com/instructorapp/companion/repository/AttendanceRepository.java
@@ -1,13 +1,12 @@
 package com.instructorapp.companion.repository;
 
 import com.instructorapp.companion.entity.Attendance;
-import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-  List<Attendance> findByMemberIdAndAttendanceDateBetween(Long memberId, LocalDate start, LocalDate end);
+  List<Attendance> findByMemberId(Long id);
 
 }
