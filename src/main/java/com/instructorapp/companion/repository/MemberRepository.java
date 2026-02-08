2f861539119477aa9707078a2e119e700721a08a
VilliDoug <villidoug@gmail.com>
2026-02-08 17:23:21 +0900

    findByActiveTrueメソッド実装・検索フィルターとして使う。

--- src/main/java/com/instructorapp/companion/repository/MemberRepository.java
+++ src/main/java/com/instructorapp/companion/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.instructorapp.companion.repository;
 
 import com.instructorapp.companion.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  List<Member> findByActiveTrue();
 
 }
