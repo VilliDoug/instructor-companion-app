0dbe963acafffca120b1ebdf6021fc1a28e842b8
VilliDoug <villidoug@gmail.com>
2026-02-15 23:03:01 +0900

    jsxからtsxへのリファクタリング。

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/main.scss';
+import MemberListPage from "./pages/MembersListPage";
+
+function App() {
+
+  return (
+    <MemberListPage />    
+  );
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
